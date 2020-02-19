46f368bdb6239d20d6677cec72dff4ae94d4c2b7
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-02-19 00:16:58 -0800

    Start working on map
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

---
+++ Voxintenebris/Assets/Scripts/PathBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathBuilder : MonoBehaviour
+{
+
+    public GameObject spawneeTile;
+    public bool stopSpawning;
+    public float spawnTime;
+    public float spawnDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+       InvokeRepeating("SpawnTile", spawnTime, spawnDelay);
+    }
+
+    public void SpawnTile() {
+        Instantiate(spawneeTile, transform.position, transform.rotation);
+        if (stopSpawning){
+            // Stop path
+        }
+    }
+
+}
