de6513619072e12a4b8c3ce857f527036b0c0422
曹景宣 <caojingxuan@169-231-107-36.wireless.ucsb.edu>
2020-02-27 15:45:09 -0800

    try again
    Signed-off-by: 曹景宣 <caojingxuan@169-231-107-36.wireless.ucsb.edu>

---
+++ Assets/handtrackinggrab.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OculusSampleFramework;
+
+public class handtrackinggrab : OVRGrabber
+{
+
+    private OVRHand hand;
+    public float pinchTreshold = 0.7f;
+    protected override void Start()
+    {
+        base.Start();
+        GetComponent<OVRHand>();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        CheckIndexPinch();
+    }
+
+    void CheckIndexPinch()
+    {
+        float pinchStrength = GetComponent<OVRHand>().GetFingerPinchStrength(OVRHand.HandFinger.Index);
+        //float pinchStrength = hand.GetFingerPinchStrength(OVRHand.HandFinger.Index);
+        //OVRLipSyncDebugConsole.print(OVRHand.HandFinger.Index);
+        bool isPinching = pinchStrength > pinchTreshold;
+
+        if(!m_grabbedObj && isPinching && m_grabCandidates.Count > 0)
+        {
+            GrabBegin();
+        }else if(m_grabbedObj && !isPinching)
+        {
+            GrabEnd();
+        }
+    }
+
+}
