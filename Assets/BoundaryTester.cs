da682b5a60bf63db1c086073b4f871b315978871
degrigis <degrigis@ucsb.edu>
2020-02-27 18:04:35 -0800

    merged door and sounds
    Signed-off-by: degrigis <degrigis@ucsb.edu>

---
+++ Assets/BoundaryTester.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+TUTORIALS:
+1- https://developer.oculus.com/reference/unity/1.42/class_o_v_r_boundary/
+2- https://github.com/NguyenRichard/VirtualWalking/blob/028f6c0fe4b912026e5876b637a5fd1f987b4ec8/Assets/VW_Assets/Scripts/Guardian/GFilterCalibrate.cs
+3- https://developer.oculus.com/reference/unity/1.42/class_o_v_r_boundary/#a5c4cb02dc5a7b8032bae6fdfc69282f0
+4- https://developer.oculus.com/documentation/native/pc/dg-guardian-system/
+5- 
+*/
+public class BoundaryTester : MonoBehaviour
+{
+    private OVRBoundary boundary;
+    private AudioSource audioData;
+    private OVRBoundary.BoundaryTestResult boundary_result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        boundary = OVRManager.boundary;
+
+        /*
+        This works!
+        */
+        /*
+        if(boundary.GetVisible()){
+            Debug.Log("Ah triggered!"); 
+            GameObject.Find("GettingOut").GetComponent<AudioSource>().Play(0);
+            GameObject.Find("RedLight").GetComponent<Light>().color =  Color.blue;
+        }else{
+            Debug.Log("Ah not triggered!");
+            GameObject.Find("GettingOut").GetComponent<AudioSource>().Pause(0);
+            GameObject.Find("RedLight").GetComponent<Light>().color =  Color.red;
+        }
+        */
+
+        OVRBoundary.BoundaryTestResult boundary_result_head = boundary.TestNode(OVRBoundary.Node.Head, OVRBoundary.BoundaryType.OuterBoundary);
+        OVRBoundary.BoundaryTestResult boundary_result_left_hand = boundary.TestNode(OVRBoundary.Node.HandLeft, OVRBoundary.BoundaryType.OuterBoundary);
+        OVRBoundary.BoundaryTestResult boundary_result_right_hand = boundary.TestNode(OVRBoundary.Node.HandRight, OVRBoundary.BoundaryType.OuterBoundary);
+
+        if(boundary_result_head.IsTriggering  || boundary_result_left_hand.IsTriggering || boundary_result_right_hand.IsTriggering ){
+            Debug.Log("Ah triggered!");
+            GameObject.Find("RedLight").GetComponent<Light>().color =  Color.blue;
+        }else{
+            Debug.Log("Ah not triggered!");
+             GameObject.Find("RedLight").GetComponent<Light>().color =  Color.red;
+        }  
+    }
+
+}
