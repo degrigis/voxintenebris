affcc1770c19895c44a1f1046e6ca89ae10c1a10
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-03-02 13:54:59 -0800

    Removed test build s
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

---
+++ Assets/AudioInput.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioInput : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private int audioSampleRate = 44100;
+    // public Transform audioSpectrumObjects;
+    public static float lerpY = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Started");
+        foreach (string device in Microphone.devices)
+        {
+            Debug.Log("Found Microphone " + device);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+        string microphone = "";
+        audioSource.clip = Microphone.Start(microphone, true, 1, audioSampleRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("Updating");
+        if (audioSource.clip == null)
+        {
+            Debug.Log("Microphone not working");
+        }
+        else
+        {
+            // Debug.Log("clip samples " + audioSource.clip.samples.ToString());
+            // Debug.Log("clip channels " + audioSource.clip.channels.ToString());
+            float[] samples = new float[audioSource.clip.samples * audioSource.clip.channels];
+            audioSource.clip.GetData(samples, 0);
+
+            float mse = 0;
+            for (int i = 0; i < samples.Length; ++i)
+            {
+                mse += samples[i] * samples[i];
+            }
+            mse = mse / samples.Length;
+            // Debug.Log("clip mse " + mse.ToString());
+            lerpY = mse * 1e3f;
+            // Debug.Log("clip lerpY " + lerpY.ToString());
+            // Vector3 newScale = new Vector3(audioSpectrumObjects.localScale.x, lerpY, audioSpectrumObjects.localScale.z);
+
+            // appply new scale to object
+            // audioSpectrumObjects.localScale = newScale;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# voxintenebris
+“The quieter you become, the more you can hear”
+
+- *App ID*: 2918293581566291 (Used to initialize the Platform SDK.)
+- *User ID*: 2693169697404384 (Your application specific user ID for debugging and testing)
+
+
+
+## Current Status
+- Game Scripting :gear:
+     - [ ] Procedurally generating the sequence of actions to escape the nightmare inside the Guardian boundaries.
+     - [X] Actions scripting
+          - [X] Opening doors. 
+          - [ ] Light up the room when player make sounds 
+
+- Game Graphics :pencil2:
+     - [X] Design a 90% dark level.
+     - [X] Designing basic objects like doors (keep it minimal)
+
+- Game Audio :sound:
+     - [ ] Understand how to do entrainment
+          - [X] Heartbeat
+          - [ ] Breath 
+          - [ ] Steps
+     - [X] Found a good soundtrack 
+     - [ ] Other 3D Audio effects 
+     - [ ] Voice input system 
+     
+- Game Plot :clapper:
+     - [X] What does the good spirit say to guide you?
+     - [X] What happen when you die?
+     - [X] What happen when you win?
