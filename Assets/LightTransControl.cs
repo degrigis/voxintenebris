ac34113267c962c5491f4cfa7c2a67554a80f9cf
yimengliu0216 <yimengliu0216@gmail.com>
2020-03-02 23:43:54 -0800

    Torch moves with camera

---
+++ Assets/LightTransControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTransControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
