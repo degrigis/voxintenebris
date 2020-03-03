1a603e42505bc9130700c42aefc86428dc8b56aa
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-03-02 16:50:35 -0800

    Fix open door
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- Assets/Navigator.cs
+++ Assets/Navigator.cs
@@ -31,15 +31,29 @@ private AudioSource audioData;
             GameObject.Find("RedLight").GetComponent<Light>().color =  Color.red;
         }
         */
+        // OVRBoundary.BoundaryTestResult boundary_result_head = boundary.TestNode(OVRBoundary.Node.Head, OVRBoundary.BoundaryType.OuterBoundary);
+        // OVRBoundary.BoundaryTestResult boundary_result_left_hand = boundary.TestNode(OVRBoundary.Node.HandLeft, OVRBoundary.BoundaryType.OuterBoundary);
+        // OVRBoundary.BoundaryTestResult boundary_result_right_hand = boundary.TestNode(OVRBoundary.Node.HandRight, OVRBoundary.BoundaryType.OuterBoundary);
+        // if(boundary_result_head.IsTriggering  || boundary_result_left_hand.IsTriggering || boundary_result_right_hand.IsTriggering ){
+        //     QuestDebug.Instance.Log("Ah triggered!");
+        //     GameObject.Find("RedLight").GetComponent<Light>().color =  Color.blue;
+        // }else{
+        //     QuestDebug.Instance.Log("Ah not triggered!");
+        //      GameObject.Find("RedLight").GetComponent<Light>().color =  Color.red;
+        // }  
+
         OVRBoundary.BoundaryTestResult boundary_result_head = boundary.TestNode(OVRBoundary.Node.Head, OVRBoundary.BoundaryType.OuterBoundary);
-        OVRBoundary.BoundaryTestResult boundary_result_left_hand = boundary.TestNode(OVRBoundary.Node.HandLeft, OVRBoundary.BoundaryType.OuterBoundary);
-        OVRBoundary.BoundaryTestResult boundary_result_right_hand = boundary.TestNode(OVRBoundary.Node.HandRight, OVRBoundary.BoundaryType.OuterBoundary);
-        if(boundary_result_head.IsTriggering  || boundary_result_left_hand.IsTriggering || boundary_result_right_hand.IsTriggering ){
-            QuestDebug.Instance.Log("Ah triggered!");
+        // OVRBoundary.BoundaryTestResult boundary_result_left_hand = boundary.TestNode(OVRBoundary.Node.HandLeft, OVRBoundary.BoundaryType.OuterBoundary);
+        // OVRBoundary.BoundaryTestResult boundary_result_right_hand = boundary.TestNode(OVRBoundary.Node.HandRight, OVRBoundary.BoundaryType.OuterBoundary);
+        if(boundary_result_head.IsTriggering){
+            // QuestDebug.Instance.Log("Ah triggered!");
             GameObject.Find("RedLight").GetComponent<Light>().color =  Color.blue;
         }else{
-            QuestDebug.Instance.Log("Ah not triggered!");
+            // QuestDebug.Instance.Log("Ah not triggered!");
              GameObject.Find("RedLight").GetComponent<Light>().color =  Color.red;
-        }  
+        }
+        boundary.SetVisible(false);
+        QuestDebug.Instance.Log(boundary_result_head.ClosestDistance.ToString());
+
     } 
 }
