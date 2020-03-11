8e5c795460a3c2c25def252a286dc9ae416f18a4
yimengliu0216 <yimengliu0216@gmail.com>
2020-03-10 21:58:13 -0700

    Update targetmaster

--- Assets/TargetManager.cs
+++ Assets/TargetManager.cs
@@ -6,7 +6,10 @@ public class TargetManager : MonoBehaviour
 {
     // Start is called before the first frame update
 
-   private Navigator navigator;
+    private Navigator navigator;
+
+    // Light lt;
+    // float setIntensity = 0.1f;
    
     /*
     When a collision is detected with a target we call the stepGame of the
@@ -20,12 +23,15 @@ public class TargetManager : MonoBehaviour
     }
     void Start()
     {
+        // lt = GetComponent<Light>();
+        // lt.intensity = setIntensity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // setIntensity = AudioInput.lerpY * 10f;
+        // lt.intensity = setIntensity;
     }
 
     public override string ToString(){
